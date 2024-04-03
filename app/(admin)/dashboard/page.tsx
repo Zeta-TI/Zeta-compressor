7815aad8c1294dfb0afdbe84d30f6fde82b6ea5a
Pedro Mota <83295376+PedrooMota@users.noreply.github.com>
2024-04-03 13:29:56 -0300

    refactor: adding responsivenes to dashboard page

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,13 +1,12 @@
 
 import Link from "next/link";
-import { BarChart } from 'lucide-react'
+import { Nfc } from 'lucide-react'
 
+import { SearchCompressorId } from "@/data/search-compressor-id";
 import { auth } from '../../../packages/auth'
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from "@/components/ui/button";
-
-import { SearchCompressorId } from "@/data/search-compressor-id";
 
 export const dynamic = "force-dynamic";
 
@@ -25,32 +24,30 @@ export default async function Dashboard() {
 
     return (
         <div>
-            <h2 className="text-3xl font-bold tracking-tight">Bem vindo, Dr(a) {user.name}</h2>
-            <div className="grid grid-cols-8 gap-4">
+            <div className="w-full max-w-6xl gap-2">
+                <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold">Bem vindo Dr(a) {session.user.name}</h1>
+            </div>
+
+            <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4 mt-2">
 
                 {compressorLogs && compressorLogs.map((item, index) => (
-                    <div className="col-span-2" key={index}>
-                        <Card>
-                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                <CardTitle className="text-base font-medium">{item.name}</CardTitle>
-                                <BarChart className="h-4 w-4 text-muted-foreground" />
-                            </CardHeader>
-                            <CardContent className="space-y-1">
-                                <span className="text-2xl font-bold">
-                                    Serial: {item.serial_number}
-                                </span>
-                                <div className="p-4">
-                                    <Button
-                                        type="submit"
-                                        variant={"outline"}
-                                        className=""
-                                    >
-                                        <Link href={`dashboard/${item.id}`}>Dashboard</Link>
-                                    </Button>
-                                </div>
-                            </CardContent>
-                        </Card>
-                    </div>
+                    <Card key={index}>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                                {item.name}
+                            </CardTitle>
+                            <Nfc className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">Serial: {item.serial_number}</div>
+                            <Button
+                                type="submit"
+                                variant={"outline"}
+                            >
+                                <Link href={`dashboard/${item.id}`}>Dashboard</Link>
+                            </Button>
+                        </CardContent>
+                    </Card>
                 ))}
             </div>
         </div>
